7a08f8e0ebd267328296384c8acffbaf14ef41bf
nicolo <nicomati1899@gmail.com>
2021-07-13 14:56:18 -0300

    proyecto cargado

---
+++ src/components/Header/header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <nav className="nav-father">
+        <div className="nav-main">
+          <img
+            className="icon-m"
+            src="https://s3-alpha-sig.figma.com/img/3025/7fe1/1feab9d1d7ac7c9cd3c4b9615b5d61f6?Expires=1625443200&Signature=hWuGww0DgpoK5bYlyZg01CdsgdR9IUqmou937j4nkhM3yDgX8cFb4iaV5bWN5gOPwHBVx88U3cXR8Gm0B4bQqVI8oBalZzVNNi-1S9thpc~UFiYP7I0yUE9MUmX4UfppFsEGxpmTKEob~8OoAkGaqXGmiywFFAkulQ4UzOPuwvIPkpJXrQKyZyGgHMpBT8iR4HtdBueRLprw6~nE5z2elWdZWmJsZMRwjDSuw3PKZH-FfXB40PrOCea2Pcv6ECEqOJdXtlp7SbNUjgciSGSk~iwTtJ9jN1OowY5ZCSEDqUfIo7IoGL5sCY7~wJOCTRbAkxZFJ8sSUKYiAcf-OWKoXQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+            alt="icon-matea"
+          ></img>
+          <a className="title-nav" href="/">Mateify</a>
+        </div>
+      </nav>
+    )
+}
+
+export default Header
